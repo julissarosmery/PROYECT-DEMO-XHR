2b0d67a126f64bd99e95d3fac72e1d4d3940c8d5
julissa <rosmery0239@gmail.com>
2018-06-20 23:13:22 -0500

    de borraron comentarios

--- index.js
+++ index.js
@@ -20,15 +20,6 @@ function getNews(){
 function handleError(){
     console.log('se ha presntado un error');
 }
-// function addNews(){
-//     const data = JSON.parse(this.responseText);//jala la function de arriba y devuelve en texto
-//     console.log(data);
-// }
-// function addNews(){
-// const data=JSON.parse(this.responseText);
-// const response= data.response;
-// console.log(response);
-// }
 function addNews(){
     const data=JSON.parse(this.responseText);
     const article=data.response.docs[0];
@@ -38,109 +29,5 @@ function addNews(){
     let li=document.createElement("li");
     li.className='articleClass';
     li.innerText=snippet;
-
     responseContainer.appendChild(li);
 }
-
-
-
-// function nanc(lama,lome){
-//     console.log('siempre biene '+lama+' quele habra pasado');
-//     console.log('siempre biene '+lome+' quele habra pasado');
-// }
-// let sugerencia=function(verbo){
-//     console.log('deberia solo '+verbo + ' la vida');
-// }
-// sugerencia('disfrutar');
-// nanc('renegando','alegre');
-
-// function hermanitos(selsin,anyelon){
-//     console.log("tengo dos de 17 años son "+selsin+anyelon);
-// }
-// let pequeñines=function(adjetivo){
-//     console.log('creo que aun son muy '+ adjetivo);
-// }
-// pequeñines('jovenes');
-// hermanitos('celso vicente jose','anyelin carme');
-
-// function celsin(edad){
-//     console.log(`oye ya tienes `+edad+` anda trabajaaa`);
-// }
-//     let años = function(colegio){
-//         console.log('terminaste hace '+colegio);
-//     }
-// años(2);
-// celsin(17);
-
-// function dia(noche,dia){
-//     console.log(`estamos en la `+noche+` asi que apurate`);
-// }
-// dia(`penumbra`);
-
-// function tiempo(edad){
-//     console.log(`hola tu edad es `+edad+` estas en el momento justo de la gloria`);
-// }
-// tiempo(27);
-
-
-// function syle(name){
-//      console.log(`hola ` +name+ ` como estas`);
-// }
-// syle(`julissa`);
-
-// function suma(){
-//     let resultado=(3+4)*2;
-//     console.log(resultado);
-// }
-// suma();
-// let suma =function(){
-//     for(let i=0;i<100;i++){
-//         console.log(i);
-//     }
-   
-// }
-// suma();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function multiplicacion(){
-//     let resultado=9*2;
-//     console.log(resultado);
-// }
-// multiplicacion();
-// for(let i=0;i<100;i++){
-//     console.log(i);
-// }
-
-// function resta(){
-//     let respuesta=10-2;
-//     console.log(respuesta);
-// }
-
-// resta();
-// for(let i =0 ;i<16;i++){
-//     console.log(i);
-// }
-
-
-// function suma(){
-//     let resultado=10;
-// }
-//     suma();
-//     for(var i =0; i<10;i++){
-//         console.log(i);
-  
-// }
-
